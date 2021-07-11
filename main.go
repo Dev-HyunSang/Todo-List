55d093ba560691596a0e83960574c812b086006f
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-07-11 16:54:43 +0900

    [FIX] log를 통한 서버 구동 확인

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"todo-list/app"
 )
 
 func main() {
+	log.Print("Listen And Server at http://localhost:3000")
 	http.ListenAndServe(":3000", app.NewHandler())
 }
